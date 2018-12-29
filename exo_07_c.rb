5c1f1fc1df1e4a4049e22b939bc2d6eb7179c91b
selmenm <selmen.mhamdi@live.fr>
2018-12-29 10:17:55 +0100

    Jeudi fini

---
+++ exo_07_c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+
+puts "Les 3 programmes font exactement la même chose en demandant une chaine de caractères et en la restituant. La différence se situe au niveau de l'intéraction avec l'utilisateur."
---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton prénom ?"
+print "> "
+user_name = gets.chomp
+puts "Cool! Et ton nom de famiille ?"
+print "> "
+user_last_name = gets.chomp
+puts "Bonjour #{user_name} #{user_last_name} !"
